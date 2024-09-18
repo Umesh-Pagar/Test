d97c2f5e08ae99384ebad09ca4490b2380048407
Umesh-Pagar-ing <Umesh.Pagar@ingenovishealth.com>
2024-09-18 18:33:01 +0530

    updates

--- wrapper/main.tf
+++ wrapper/main.tf
@@ -47,53 +47,9 @@ resource "azapi_update_resource" "enable_soft_delete" {
   })
 }
 
-resource "azurerm_user_assigned_identity" "aks_identity" {
+resource "azurerm_search_service" "srch" {
+  name                = "srch-${local.location}"
   resource_group_name = local.resource_group_name
   location            = local.location
-  name                = "aks-identity"
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                    = "aks-${local.location}"
-  location                = local.location
-  resource_group_name     = local.resource_group_name
-  dns_prefix              = "aks-${local.location}"
-  private_cluster_enabled = false
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "UserAssigned"
-    identity_ids = [
-      azurerm_user_assigned_identity.aks_identity.id
-    ]
-  }
-
-  tags = local.tags
-}
-
-resource "azurerm_kubernetes_cluster_extension" "extensions" {
-  name = "flux"
-  cluster_id = azurerm_kubernetes_cluster.aks.id
-  extension_type = "microsoft.flux"
-
-  configuration_settings = {
-    "helm-controller.enabled" : "true"
-    "source-controller.enabled" : "true"
-    "kustomize-controller.enabled" : "true"
-    "notification-controller.enabled" : "true"
-    "image-automation-controller.enabled" : "true"
-    "image-reflector-controller.enabled" : "true"
-  }
-}
-
-
-resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  sku                 = "standard"
 }
