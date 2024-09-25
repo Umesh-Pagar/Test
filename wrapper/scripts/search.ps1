4d565994e51885437637d69581646c7bdc52a1f9
Umesh-Pagar-ing <Umesh.Pagar@ingenovishealth.com>
2024-09-25 14:22:18 +0530

    Added

--- wrapper/scripts/search.ps1
+++ wrapper/scripts/search.ps1
@@ -1,10 +1,16 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string]$rid
+    [string]$subid
+
+    [string]rg
+
+    [string]name
 )
 
+
+
 # Construct the URI
-$uri = "https://management.azure.com${rid}?api-version=2024-03-01-Preview"
+$uri = "https://management.azure.com/subscriptions/$subid/resourceGroups/$rg/providers/Microsoft.Search/searchServices/$name?api-version=2024-03-01-Preview"
 
 # Use Azure CLI to get the resource details and save to search.json
 az rest --uri $uri > search.json
