e06e6a34607ced99330c2397ee2d8b4f6ee463b3
Umesh-Pagar-ing <Umesh.Pagar@ingenovishealth.com>
2024-12-10 12:37:38 +0530

    Added

--- wrapper/main.tf
+++ wrapper/main.tf
@@ -10,43 +10,6 @@ module "virtual_network" {
   tags = local.tags
 }
 
-# resource "azurerm_user_assigned_identity" "acr_identity" {
-#   resource_group_name = local.resource_group_name
-#   location            = local.location
-#   name                = "acr-identity"
-# }
-
-# resource "azurerm_container_registry" "acr" {
-#   name                          = "acractions${local.location}"
-#   resource_group_name           = local.resource_group_name
-#   location                      = local.location
-#   sku                           = "Standard"
-#   public_network_access_enabled = true
-#   admin_enabled                 = true
-#   identity {
-#     type = "UserAssigned"
-#     identity_ids = [
-#       azurerm_user_assigned_identity.acr_identity.id
-#     ]
-#   }
-#   tags = local.tags
-# }
-
-# resource "azapi_update_resource" "enable_soft_delete" {
-#   resource_id = azurerm_container_registry.acr.id
-#   type        = "Microsoft.ContainerRegistry/registries@2023-01-01-preview"
-#   body = jsonencode({
-#     properties = {
-#       policies = {
-#         softDeletePolicy = {
-#           retentionDays = var.soft_delete_retention_days
-#           status        = var.soft_delete_enabled ? "Enabled" : "Disabled"
-#         }
-#       }
-#     }
-#   })
-# }
-
 resource "azurerm_search_service" "srch" {
   name                = "srch-${local.location}"
   resource_group_name = local.resource_group_name
